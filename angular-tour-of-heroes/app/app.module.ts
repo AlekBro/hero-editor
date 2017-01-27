235faa9a7e24f19fc5037bcea9ac19febd72efdc
AleksBro <aleksandr.brova@yandex.ru>
2017-01-27 17:43:57 +0200

    REDIRECTTO

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -18,6 +18,11 @@ import { DashboardComponent }        from './dashboard.component';
         FormsModule,
         RouterModule.forRoot([
             {
+                path: '',
+                redirectTo: '/dashboard',
+                pathMatch: 'full'
+            },
+            {
                 path: 'heroes',
                 component: HeroesComponent
             },
